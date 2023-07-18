f1dc7b5019bef33f83051f68b73abd7ad26d0d81
aqua <norbertzieba06@gmail.com>
2023-07-18 06:33:47 -0400

    fix: google auth timeout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ function AuthShowcase() {
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={
-          sessionData ? () => void signOut() : () => void signIn()
+          sessionData ? () => signOut() : async () => await signIn()
         }
       >
         {sessionData ? "Sign out" : "Sign in"}
