1fab71bb16ed077d9513d989feb5422541e67dfb
aqua <norbertzieba06@gmail.com>
2023-07-18 07:52:33 -0400

    fix: emails linking

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -54,10 +54,12 @@ export const authOptions: NextAuthOptions = {
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
     /**
      * ...add more providers here.
